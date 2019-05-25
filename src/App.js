e495fa0824cc3078185b78cedc4bdaab013a213c
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2019-05-24 21:17:13 -0700

    some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
+import React from 'react';
+import styled from 'styled-components';
+import { Root, Routes, addPrefetchExcludes } from 'react-static';
 //
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
+import { Link, Router } from 'components/Router';
+import Dynamic from 'containers/Dynamic';
 
-import './app.css'
+import './app.css';
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
+addPrefetchExcludes(['dynamic']);
+
+const StyledNav = styled.nav`
+  width: 100%;
+  border-bottom: 1px solid #eee;
+`;
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  color: #108db8;
+  padding: 1rem 2rem;
+  text-decoration: none;
+`;
 
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
+      <StyledNav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/blog">Blog</StyledLink>
+      </StyledNav>
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
@@ -27,7 +38,7 @@ function App() {
         </React.Suspense>
       </div>
     </Root>
-  )
+  );
 }
 
-export default App
+export default App;
