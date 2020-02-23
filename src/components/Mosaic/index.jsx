53e3d3eba8de2e9c8af2734ec1742930b78e1a27
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2020-02-23 13:09:16 -0800

    load a number of files; better code organization

--- src/components/Mosaic/index.jsx
+++ src/components/Mosaic/index.jsx
@@ -1,47 +1,45 @@
-import React, { useRef, useCallback } from 'react';
+import React from 'react';
 import { fabric } from 'fabric';
-
-// https://stackoverflow.com/questions/37565041/how-can-i-use-fabric-js-with-react
-const useFabric = (onChange) => {
-    const fabricRef = useRef();
-    const disposeRef = useRef();
-    return useCallback((node) => {
-        if (node) {
-            fabricRef.current = new fabric.Canvas(node);
-            if (onChange) {
-                disposeRef.current = onChange(fabricRef.current);
-            }
-        }
-        else if (fabricRef.current) {
-            fabricRef.current.dispose();
-            if (disposeRef.current) {
-                disposeRef.current();
-                disposeRef.current = undefined;
-            }
-        }
-    }, []);
-};
+import useFabric from './useFabric';
 
 export const Mosaic = () => {
-    const ref = useFabric((fabricCanvas) => {
-        console.log('fabricCanvas: ', fabricCanvas);
-        // fabric.Image.fromURL('images/mosaic/0,6.jpg', (loadedImg) => {
-        //     loadedImg.scale(0.2);
-        //     fabricCanvas.add(loadedImg);
-        // });
-    });
-
-    
-    // return null;
-    return (
-        <canvas
-            ref={ref}
-            style={{
-                width: '100%',
-                height: '80vh',
-            }}
-        />
+    // I could have done
+    // const ref = useFabric(doTheMosaic)
+    // but then, the fact that fabricCanvas is passed gets lost. This way is probably better for readability
+    const ref = useFabric((fabricCanvas) =>
+        doTheMosaic(fabricCanvas)
     );
+
+    return (
+        <canvas ref={ref} />
+    );
+};
+
+export const doTheMosaic = (fabricCanvas) => {
+    console.log('fabricCanvas: ', fabricCanvas);
+    setCanvasSize(fabricCanvas);
+    loadFilesIntoCanvas(fabricCanvas);
+    fabricCanvas.renderAll();
+}
+
+export const setCanvasSize = (fabricCanvas) => {
+    fabricCanvas.setHeight(600);
+    fabricCanvas.setWidth(800);
+};
+
+export const loadFilesIntoCanvas = (fabricCanvas) => {
+    const Y_MIN = 0;
+    const Y_MAX = 4;
+    const PATH = 'images/mosaic/';
+    const SCALE = 0.2;
+
+    for (let yCoord = Y_MIN; yCoord <= Y_MAX; yCoord += 1) {
+        const file = `${PATH}0,${yCoord}.jpg`;
+        fabric.Image.fromURL(file, (loadedImg) => {
+            loadedImg.scale(SCALE);
+            fabricCanvas.add(loadedImg);
+        });
+    }
 };
 
 export default Mosaic;
