9b8fbb729a9bfc8b8824d1afc3cb4a7d902f4364
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2020-02-23 22:29:25 -0800

    get a mosaic grid going

--- src/components/Mosaic/index.jsx
+++ src/components/Mosaic/index.jsx
@@ -3,11 +3,32 @@ import { fabric } from 'fabric';
 import useFabric from './useFabric';
 
 export const Mosaic = () => {
+    const MOSAIC_DATA = {
+        bounds: {
+            xMin: 0,
+            xMax: 4,
+            yMin: 6,
+            yMax: 6,
+        },
+        pieceDimensions: {
+            width: 5,
+            height: 3,
+            multiplier: 30,
+        },
+        pieces: {},
+        images: {
+            path: '/images/mosaic/',
+        },
+        canvas: {
+            scale: 0.1,
+        },
+    };
+
     // I could have done
     // const ref = useFabric(doTheMosaic)
     // but then, the fact that fabricCanvas is passed gets lost. This way is probably better for readability
     const ref = useFabric((fabricCanvas) =>
-        doTheMosaic(fabricCanvas)
+        doTheMosaic(fabricCanvas, MOSAIC_DATA)
     );
 
     return (
@@ -15,31 +36,67 @@ export const Mosaic = () => {
     );
 };
 
-export const doTheMosaic = (fabricCanvas) => {
-    console.log('fabricCanvas: ', fabricCanvas);
+const doTheMosaic = (fabricCanvas, mosaicData) => {
+    console.log('doTheMosaic > fabricCanvas: ', fabricCanvas);
+
     setCanvasSize(fabricCanvas);
-    loadFilesIntoCanvas(fabricCanvas);
+    loadFilesIntoCanvas(fabricCanvas, mosaicData);
     fabricCanvas.renderAll();
 }
 
-export const setCanvasSize = (fabricCanvas) => {
+const setCanvasSize = (fabricCanvas) => {
     fabricCanvas.setHeight(600);
     fabricCanvas.setWidth(800);
 };
 
-export const loadFilesIntoCanvas = (fabricCanvas) => {
-    const Y_MIN = 0;
-    const Y_MAX = 4;
-    const PATH = 'images/mosaic/';
-    const SCALE = 0.2;
+const loadFilesIntoCanvas = (fabricCanvas, mosaicData) => {
+    const { bounds } = mosaicData;
+    const { xMin, xMax, yMin, yMax } = bounds;
+    // const { scale } = canvas;
 
-    for (let yCoord = Y_MIN; yCoord <= Y_MAX; yCoord += 1) {
-        const file = `${PATH}0,${yCoord}.jpg`;
-        fabric.Image.fromURL(file, (loadedImg) => {
-            loadedImg.scale(SCALE);
-            fabricCanvas.add(loadedImg);
-        });
+    console.log('loadFilesIntoCanvas > bounds: ', bounds);
+
+    for (let xGrid = xMin; xGrid <= xMax; xGrid += 1) {
+        console.log('=====\nxGrid: ', xGrid);
+        for (let yGrid = yMax; yGrid >= yMin; yGrid -= 1) {
+            console.log('-----\nyGrid: ', yGrid);
+            loadAndPlacePiece(xGrid, yGrid, fabricCanvas, mosaicData);
+        }
     }
+};
+
+const loadAndPlacePiece = (xGrid, yGrid, fabricCanvas, mosaicData) => {
+    console.log('loadAndPlacePiece > xGrid: ', xGrid, ', yGrid: ', yGrid);
+    const { pieces, pieceDimensions, images, bounds } = mosaicData;
+    const { width, multiplier } = pieceDimensions;
+    const { path } = images;
+
+    const { xMin, yMin, yMax } = bounds;
+
+    const xRef = (xGrid + xMin);
+
+    const yRange = yMax - yMin;
+    const yRef = yRange - yGrid + yMin; // these go in inverse order
+    // const yRef = 0;
+    console.log('loadAndPlacePiece > xRef: ', xRef, ', yRef: ', yRef);
+
+    const gridCoords = `${xGrid},${yGrid}`;
+    const file = `${path}${gridCoords}.jpg`;
+    fabric.Image.fromURL(file, (loadedImg) => {
+        pieces[gridCoords] = loadedImg; // eg. pieces['2,3']
+        // loadedImg.scale(scale);
+
+        const pixelWidth = width * multiplier;
+        loadedImg.scaleToWidth(pixelWidth, false)
+
+        const xInCanvas = xRef * pixelWidth;
+        loadedImg.set('left', xInCanvas);
+
+        const yInCanvas = yRef * pixelWidth;
+        loadedImg.set('top', yInCanvas);
+
+        fabricCanvas.add(loadedImg);
+    });
 };
 
 export default Mosaic;
