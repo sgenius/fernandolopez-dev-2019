2f37ce572dec4cd09f9173e82b81595c92a98cad
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2020-02-29 23:27:14 -0800

    load different image resolutions depending on the zoom
    for now everything loads again; gotta create a buffer
    also need to give max bounds to panning

--- src/components/Mosaic/index.jsx
+++ src/components/Mosaic/index.jsx
@@ -5,8 +5,8 @@ import useFabric from './useFabric';
 export const Mosaic = () => {
     const MOSAIC_DATA = {
         bounds: {
-            xMin: -10,
-            xMax: 10,
+            xMin: -3,
+            xMax: 12,
             yMin: -17,
             yMax: 9,
         },
@@ -16,16 +16,23 @@ export const Mosaic = () => {
             multiplier: 10,
         },
         pieces: {},
-        images: {
-            path: '/images/mosaic/thumb_',
-        },
+        imageSets: [
+            {
+                minZoom: 0,
+                path: '/images/mosaic/thumb_',
+            },
+            {
+                minZoom: 3,
+                path: '/images/mosaic/',
+            },
+        ],
+        currentImageSet: 0,
         canvas: {
             zoomConfig: {
                 step: 200,
                 min: 0.01,
                 max: 20,
             },
-
         },
     };
 
@@ -59,8 +66,11 @@ const setUpZoom = (fabricCanvas, mosaicData) => {
         if (zoom < min) {
             zoom = min;
         }
-        
+
+        console.log('zoom: ', zoom);
         fabricCanvas.zoomToPoint({x: opt.e.offsetX, y: opt.e.offsetY}, zoom);
+
+        updateCurrentImageSetByZoom(fabricCanvas, mosaicData);
 
         opt.e.preventDefault();
         opt.e.stopPropagation();
@@ -70,19 +80,19 @@ const setUpZoom = (fabricCanvas, mosaicData) => {
 const setUpDrag = (fabricCanvas, mosaicData) => {
     fabricCanvas.on('mouse:down', function(opt) {
         const evt = opt.e;
-        console.log('evt: ', evt);
         if (evt.altKey === true) {
             this.isDragging = true;
             this.selection = false;
             this.lastPosX = evt.clientX;
             this.lastPosY = evt.clientY;
-            console.log('this.isDragging: ', this.isDragging);
         }
     });
 
     fabricCanvas.on('mouse:move', function(opt) {
+        const evt = opt.e;
+        const pointer = fabricCanvas.getPointer(evt, false);
+        console.log('mouse at: ', pointer);
         if(this.isDragging) {
-            const evt = opt.e;
             // viewportTransform has the format of canvas.transform:
             // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/transform
             this.viewportTransform[4] += evt.clientX - this.lastPosX;
@@ -114,31 +124,24 @@ const loadFilesIntoCanvas = (fabricCanvas, mosaicData) => {
     const { bounds } = mosaicData;
     const { xMin, xMax, yMin, yMax } = bounds;
 
-    console.log('bounds: ', bounds);
-
     for (let xGrid = xMin; xGrid <= xMax; xGrid += 1) {
         for (let yGrid = yMax; yGrid >= yMin; yGrid -= 1) {
-            loadPiece(xGrid, yGrid, fabricCanvas, mosaicData, () => {
-                placePiece(xGrid, yGrid, fabricCanvas, mosaicData);
-            });
-            // loadAndPlacePiece(xGrid, yGrid, fabricCanvas, mosaicData);
+            loadPiece(xGrid, yGrid, fabricCanvas, mosaicData);
         }
     }
 };
 
-const loadPiece = (xGrid, yGrid, fabricCanvas, mosaicData, callback) => {
-    const { pieces, images } = mosaicData;
-    const { path } = images;
-
+const loadPiece = (xGrid, yGrid, fabricCanvas, mosaicData) => {
+    const { pieces, imageSets, currentImageSet } = mosaicData;
     const gridCoords = `${xGrid},${yGrid}`;
+    const path = imageSets[currentImageSet].path;
     const file = `${path}${gridCoords}.jpg`;
 
     fabric.Image.fromURL(file, (loadedImg) => {
         pieces[gridCoords] = loadedImg; // eg. pieces['2,3']
+        loadedImg.selectable = false;
         fabricCanvas.add(loadedImg);
-        if(callback) {
-            callback();
-        }
+        placePiece(xGrid, yGrid, fabricCanvas, mosaicData);
     });
 }
 
@@ -153,15 +156,11 @@ const placePiece = (xGrid, yGrid, fabricCanvas, mosaicData) => {
 
     const gridCoords = `${xGrid},${yGrid}`;
     
-    // console.log('loadAndPlacePiece(xGrid: ', xGrid, ', yGrid: ', yGrid, '):');
-    // console.log('   xRef: ', xRef, ', yRef: ', yRef);
-
     const loadedImg = pieces[gridCoords];
-    fabricCanvas.remove(loadedImg);
 
     const pixelWidth = width * multiplier;
     const pixelHeight = height * multiplier;
-    loadedImg.scaleToWidth(pixelWidth, false)
+    loadedImg.scaleToWidth(pixelWidth, true)
 
     const xInCanvas = xRef * pixelWidth;
     loadedImg.set('left', xInCanvas);
@@ -169,12 +168,36 @@ const placePiece = (xGrid, yGrid, fabricCanvas, mosaicData) => {
     const yInCanvas = yRef * pixelHeight;
     loadedImg.set('top', yInCanvas);
 
-    // console.log('   For gridCoords ', gridCoords, ': xInCanvas: ', xInCanvas, ', yInCanvas, ', yInCanvas);
-
     // https://github.com/fabricjs/fabric.js/wiki/When-to-call-setCoords
     loadedImg.setCoords();
 
     fabricCanvas.add(loadedImg);
 };
+
+const updateCurrentImageSetByZoom = (fabricCanvas, mosaicData) => {
+    const { imageSets, currentImageSet } = mosaicData;
+    const zoom = fabricCanvas.getZoom();
+    let zoomChanged = false;
+
+    console.log('updateCurrentImageSetByZoom > currentImageSet: ', currentImageSet, ', imageSets.length: ', imageSets.length);
+    if (currentImageSet < imageSets.length - 1 && imageSets[currentImageSet + 1].minZoom < zoom) {
+        mosaicData.currentImageSet += 1;
+        zoomChanged = true;
+    }
+
+    if (currentImageSet > 0 && imageSets[currentImageSet].minZoom > zoom) {
+        mosaicData.currentImageSet -= 1;
+        zoomChanged = true;
+    }
+
+    if (zoomChanged) {
+        reloadPieces(fabricCanvas, mosaicData);
+    }
+}
+
+const reloadPieces = (fabricCanvas, mosaicData) => {
+    loadFilesIntoCanvas(fabricCanvas, mosaicData);
+    fabricCanvas.renderAll();
+}
 
 export default Mosaic;
