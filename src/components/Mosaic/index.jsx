ae96880a5431adb6519bb978e104bbba1e1f7266
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2020-11-08 18:45:35 -0800

    zoom with touch now works

--- src/components/Mosaic/index.jsx
+++ src/components/Mosaic/index.jsx
@@ -9,10 +9,6 @@ export const Mosaic = () => {
         x: 0,
         y: 0,
     });
-    const [mapCoords, setMapCoords] = useState({
-        x: 0,
-        y: 0,
-    });
     const [zoom, setZoom] = useState(0);
 
     const ref = useFabric((fabricCanvas) => {
@@ -34,17 +30,13 @@ export const Mosaic = () => {
  };
 
 const setUpCanvasEvents = (fabricCanvas, mosaicData, setMouseCoords, setZoom) => {
-    setUpZoom(fabricCanvas, mosaicData, setZoom);
-    setUpDrag(fabricCanvas, mosaicData, setMouseCoords);
-};
-
-const setUpZoom = (fabricCanvas, mosaicData, setZoom) => {
     fabricCanvas.on('mouse:wheel', function(opt) {
+        const evt = opt.e;
         const { canvas } = mosaicData;
         const { zoomConfig } = canvas;
         const { step, max, min } = zoomConfig;
 
-        const delta = opt.e.deltaY;
+        const delta = evt.deltaY;
         let zoom = fabricCanvas.getZoom();
         zoom = zoom + delta/step;
         if (zoom > max) {
@@ -54,74 +46,147 @@ const setUpZoom = (fabricCanvas, mosaicData, setZoom) => {
             zoom = min;
         }
 
-        fabricCanvas.zoomToPoint({x: opt.e.offsetX, y: opt.e.offsetY}, zoom);
+        fabricCanvas.zoomToPoint({x: evt.offsetX, y: evt.offsetY}, zoom);
 
         updateCurrentImageSetByZoom(fabricCanvas, mosaicData);
         setZoom(zoom);
 
-        opt.e.preventDefault();
-        opt.e.stopPropagation();
+        evt.preventDefault();
+        evt.stopPropagation();
     });
-};
 
-const setUpDrag = (fabricCanvas, mosaicData, setMouseCoords) => {
     fabricCanvas.on('mouse:down', function(opt) {
         const evt = opt.e;
         this.isDragging = true;
         this.selection = false;
-        if (evt.clientX !== undefined) {
-            this.lastPosX = evt.clientX;
-            this.lastPosY = evt.clientY;
-        } else {
-            this.lastPosX = evt.targetTouches[0].clientX;
-            this.lastPosY = evt.targetTouches[0].clientY;
+        const clientCoords = getClientCoordsFromEvent(evt);
+        this.lastPosX = clientCoords.x;
+        this.lastPosY = clientCoords.y;
+        
+        // if this is actually a touch event, store all of the current touches.
+        if (evt.touches) {
+            this.touches = evt.touches;
+            console.log('fabricCanvas.on(mouse:down) > touches: ', this.touches);
         }
     });
 
     fabricCanvas.on('mouse:move', function(opt) {
         const evt = opt.e;
-        let pointer = {
-            x: undefined,
-            y: undefined,
-        };
-        if (evt.clientX !== undefined) {
-            // this is a mouse event
-            pointer = {
-                x: evt.clientX,
-                y: evt.clientY,
-            };
-        } else {
-            // this is a touch event
-            pointer = {
-                x: evt.targetTouches[0].clientX,
-                y: evt.targetTouches[0].clientY,
-            };
-        }
+        const pointer = getClientCoordsFromEvent(evt);
         setMouseCoords(pointer);
-        if(this.isDragging) {
-            console.log('setUpDrag > this.viewportTransform (before): ', this.viewportTransform);
-            console.log('setUpDrag > pointer.x: ', pointer.x);
-            console.log('setUpDrag > this.lastPosX: ', this.lastPosX);
 
-            // viewportTransform has the format of canvas.transform:
-            // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/transform
-            this.viewportTransform[4] += pointer.x - this.lastPosX;
-            this.viewportTransform[5] += pointer.y - this.lastPosY;
-            this.requestRenderAll();
-            console.log('setUpDrag > this.viewportTransform (after): ', this.viewportTransform);
+        console.log('fabricCanvas.on(mouse:move) > evt.touches: ', evt.touches);
+        console.log('fabricCanvas.on(mouse:move) > this.touches: ', this.touches);
 
-            this.lastPosX = pointer.x;
-            this.lastPosY = pointer.y;
+        if (this.touches && this.touches.length > 1 && evt.touches && evt.touches.length > 1) {
+            console.log('fabricCanvas.on(mouse:move) > about to zoom?')
+            // for (var i = 0; i < evt.touches.length; i++) {
+            //     const touch = evt.touches.item(i);
+            //     console.log('fabricCanvas.on(mouse:move) > touch #', i, ': ', touch);
+            // }
+            // do zoom using the two first registered touches
 
+            // first, get the delta: 
+            // - get the distance between the original points
+            // - get the distance between the dragged points
+            // - the difference is the delta
+            const { canvas } = mosaicData;
+            const { zoomConfig } = canvas;    
+            const { step, max, min } = zoomConfig;
+
+            const oldPoint0 = getClientCoordsFromEvent(this.touches.item(0));
+            const oldPoint1 = getClientCoordsFromEvent(this.touches.item(1));
+
+            const newPoint0 = getClientCoordsFromEvent(evt.touches.item(0));
+            const newPoint1 = getClientCoordsFromEvent(evt.touches.item(1));
+
+            const originalDistance = getDistanceBetweenPoints(oldPoint0, oldPoint1);
+            const newDistance = getDistanceBetweenPoints(newPoint0, newPoint1);
+            console.log('fabricCanvas.on(mouse:move) > originalDistance: ', originalDistance);
+            console.log('fabricCanvas.on(mouse:move) > newDistance: ', newDistance);
+
+            const delta = newDistance - originalDistance;
+            let zoom = fabricCanvas.getZoom();
+            zoom = zoom + delta / step;
+            if (zoom > max) {
+                zoom = max;
+            }
+            if (zoom < min) {
+                zoom = min;
+            }
+
+            // the center point of the zoom is the middle point between the two *last* touches
+            const pointToZoom = getMiddlePoint(newPoint0, newPoint1);
+
+            fabricCanvas.zoomToPoint(pointToZoom, zoom);
+
+            updateCurrentImageSetByZoom(fabricCanvas, mosaicData);
+            setZoom(zoom);
+    
+            evt.preventDefault();
+            evt.stopPropagation();
+
+            // finally, update the touches saved in the fabricCanvas object
+            this.touches = evt.touches;
+        } else if(this.isDragging) {
+            doDrag(this, pointer);
+        }
+
+        if (evt.touches) {
+            this.touches = evt.touches;
         }
        
     });
 
     fabricCanvas.on('mouse:up', function(opt) {
+        console.log('fabricCanvas.on(mouse:up)');
+        this.touches = undefined;
         this.isDragging = false;
         this.selection = true;
     });
 };
+
+const getDistanceBetweenPoints = (point1, point2) => {
+    console.log('getDistanceBetweenPoints > point1: ', point1);
+    return Math.sqrt(Math.pow((point2.y - point1.y), 2) + Math.pow((point2.x - point1.x), 2));
+}
+
+const getMiddlePoint = (point1, point2) => {
+    return {
+        x: (point1.x + point2.x) / 2,
+        y: (point1.y + point2.y) / 2,
+    };
+}
+
+const getClientCoordsFromEvent = (evt) => {
+    if (evt.clientX !== undefined) {
+        return {
+            x: evt.clientX,
+            y: evt.clientY,
+        }
+    } else if (evt.targetTouches[0].clientX !== undefined) {
+        return {
+            x: evt.targetTouches[0].clientX,
+            y: evt.targetTouches[0].clientY,
+        }
+    };
+    return {
+        x: undefined,
+        y: undefined,
+    };
+}
+
+const doDrag = (fabricCanvas, clientCoords) => {
+    // viewportTransform has the format of canvas.transform:
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/transform
+    fabricCanvas.viewportTransform[4] += clientCoords.x - fabricCanvas.lastPosX;
+    fabricCanvas.viewportTransform[5] += clientCoords.y - fabricCanvas.lastPosY;
+    fabricCanvas.requestRenderAll();
+    fabricCanvas.lastPosX = clientCoords.x;
+    fabricCanvas.lastPosY = clientCoords.y;
+}
+
+
 
 const doTheMosaic = (fabricCanvas, mosaicData) => {
     setCanvasSize(fabricCanvas);
@@ -217,8 +282,6 @@ const unloadAllPieces = (fabricCanvas, mosaicData) => {
             unloadPiece(xGrid, yGrid, fabricCanvas, mosaicData);
         }
     }
-
-    // mosaicData.pieces = {};
 }
 
 const unloadPiece = (xGrid, yGrid, fabricCanvas, mosaicData) => {
